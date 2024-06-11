d1788cdfa77a5b008d8e2997a983ae34d76fffa6
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2024-06-11 18:24:37 -0300

    axios implement

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const name = "ian";
+  const surname = "sakashita";
+
+  const handleClick = async () => {
+    const response = await axios.get('https://backend-learning-styles.vercel.app/')
+    console.log(response)
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          O {name} casou com o {surname}
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <button onClick={handleClick}>
+          Click
+        </button>
+        </div>
     </>
   )
 }
